9144a8b6ae0fc17a7c70b8d7cfe1a6a94917cd8b
Eduardo Lazarini Fontana <edulfontana@gmail.com>
2023-10-15 05:37:24 -0300

    feat: backend partial validation + FIlter

--- src/main/java/br/com/eduardofontana/todolist/user/UserController.java
+++ src/main/java/br/com/eduardofontana/todolist/user/UserController.java
@@ -1,17 +1,34 @@
 package br.com.eduardofontana.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public String create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.username);
-        return "ok";
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(400).body("User already exists");
+        }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(201).body(userCreated);
     }
 }
